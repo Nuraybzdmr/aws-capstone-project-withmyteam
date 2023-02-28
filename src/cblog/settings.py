9e4800d2aa592ca1adbe82e74413d8cd4cadd86e
Nuraybzdmr <seyyahatlerce@gmail.com>
2023-02-28 22:40:56 +0300

    added

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'database1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
-        'HOST': 'database1.cdrtjgld9koi.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'HOST': 'aws-capstone-rds.cdrtjgld9koi.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
@@ -145,7 +145,7 @@ LOGIN_URL = "login"
 
 AWS_STORAGE_BUCKET_NAME = 'nurayblog' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_REGION_NAME = "" # please enter your s3 region 
+AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
 
 AWS_LOCATION = 'static'
