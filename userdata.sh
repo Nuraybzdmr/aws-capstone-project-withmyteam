4778f007d9b6d6ec93f952159db29e0868326f02
Nuraybzdmr <seyyahatlerce@gmail.com>
2023-03-02 15:12:12 +0300

    added outputs image

--- userdata.sh
+++ userdata.sh
@@ -3,9 +3,16 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
+<<<<<<< HEAD
 TOKEN=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
 git clone https://$TOKEN@github.com/Nuraybzdmr/aws-capstone-project-withmyteam.git
 cd /home/ubuntu/aws-capstone-project-withmyteam
+=======
+TOKEN= xxxxxxxxx
+git clone https://$TOKEN@github.com/Nuraybzdmr/aws-capstone-project-withmyteam
+cd /home/ubuntu/
+aws-capstone-project-withmyteam
+>>>>>>> 0ff025041d1add6509d38542ca2e09f09ed31467
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
