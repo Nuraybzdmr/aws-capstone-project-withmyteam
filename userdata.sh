bff05174516a4f624b1cb87985802baa10d0beaf
Nuraybzdmr <seyyahatlerce@gmail.com>
2023-03-02 13:55:47 +0300

    project output images added

--- userdata.sh
+++ userdata.sh
@@ -3,14 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN=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
-git clone https://$TOKEN@github.com/Nuraybzdmr/aws-capstone-project-withmyteam
-cd /home/ubuntu/
-aws-capstone-project-withmyteam
+TOKEN=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
+git clone https://$TOKEN@github.com/Nuraybzdmr/aws-capstone-project-withmyteam.git
+cd /home/ubuntu/aws-capstone-project-withmyteam
 apt install python3-pip -y
-apt-get install python3.7-dev default-libmysqlclient-dev -y
+apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/my-aws-capstone-project/src
+cd /home/ubuntu/aws-capstone-project-withmyteam/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
